f79119106d93a80bc18d66e35baaed5697213aec
Hasindu <hlrathnayaka@gmail.com>
2020-05-19 00:48:17 +0530

    Add project files.

---
+++ BlazorDroid.Droid/ServerInvoker.cs
@@ -0,0 +1,45 @@
+﻿using Android.OS;
+using BlazorDroid;
+using System;
+using System.IO;
+
+public class ServerInvoker
+{
+    Android.Content.Res.AssetManager Assets;
+    string basePath;
+    public void Start(Android.Content.Res.AssetManager ass)
+    {
+        Assets = ass;
+        string root = "Blazite/wwwroot/";
+        basePath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData);
+        RecursiveCopy(root);
+        string fp = Path.GetFullPath(basePath);
+        SimpleHTTPServer svr = new SimpleHTTPServer(Path.Combine(basePath, root), 6961);
+    }
+
+
+    private void RecursiveCopy(string path)
+    {
+        string[] lst = Assets.List(path);
+
+        foreach (string file in lst)
+        {
+            string filepath = Path.Combine(path, file);
+            try
+            {
+                Stream src = Assets.Open(filepath);
+                Stream dest = File.OpenWrite(Path.Combine(basePath, filepath));
+                src.CopyTo(dest);
+                src.Flush();
+                dest.Close();
+            }
+            catch (Java.IO.FileNotFoundException)
+            {
+                Directory.CreateDirectory(Path.Combine(basePath, filepath));
+                RecursiveCopy(filepath);
+            }
+        }
+
+
+    }
+}
---
+++ BlazorDroid.Tester/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace BlazorDroid.Tester
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+
+            SimpleHTTPServer svr = new SimpleHTTPServer(Path.GetFullPath("wwwroot"), 6961);
+            Console.ReadLine();
+        }
+    }
+}
