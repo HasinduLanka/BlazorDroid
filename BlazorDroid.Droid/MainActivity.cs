4e4e606223d12b556465ed9e2b5b71652eb0176c
harindu wijesinghe <hariwijesinghe234@gmail.com>
2020-06-29 08:48:37 +0530

    Revert "Android Sample"
    This reverts commit dd6207105b3941ba16df8645188743a62e486a0b.

--- BlazorDroid.Droid/MainActivity.cs
+++ BlazorDroid.Droid/MainActivity.cs
@@ -6,7 +6,6 @@ using Android.Runtime;
 using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
-using Android.Webkit;
 using Android.Widget;
 using Xamarin.Essentials;
 
@@ -15,9 +14,10 @@ namespace BlazorDroid.Droid
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        private static WebView web;// WebView In App
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
+
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
@@ -25,57 +25,61 @@ namespace BlazorDroid.Droid
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
 
-            new System.Threading.Thread(new System.Threading.ThreadStart(StartServer)) { Name = "ServerStart" }.Start();
-            System.Threading.Thread.Sleep(200);
-            web = FindViewById<WebView>(Resource.Id.webView1);
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
 
-            //LaunchInBrowser(); // Open In Default Web Browser
-            LaunchInApp();//Open In Application
+
+            new System.Threading.Thread(new System.Threading.ThreadStart(StartServer)) { Name = "ServerStart" }.Start();
+
+            System.Threading.Thread.Sleep(200);
+
+            LaunchBrowser();
+
         }
-        private static void LaunchInBrowser()
+
+        private static void LaunchBrowser()
         {
             Xamarin.Essentials.Browser.OpenAsync("http://127.0.0.1:6961", new BrowserLaunchOptions() { TitleMode = BrowserTitleMode.Hide });
         }
-        private static void LaunchInApp()
-        {
-            if (web != null)
-            {
-                web.LoadUrl("http://127.0.0.1:6961");
-            }
-        }
+
         private void StartServer()
         {
+
             ServerInvoker invoker = new ServerInvoker();
             invoker.Start(Assets);
         }
+
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
             MenuInflater.Inflate(Resource.Menu.menu_main, menu);
             return true;
         }
+
         public override bool OnOptionsItemSelected(IMenuItem item)
         {
             int id = item.ItemId;
-            if (id == Resource.Id.action_opb)
+            if (id == Resource.Id.action_settings)
             {
-                Snackbar.Make(CurrentFocus, "Opening In Browser...", Snackbar.LengthLong).SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
-                LaunchInBrowser();
                 return true;
             }
-            if (id == Resource.Id.action_ref)
-            {
-                if (web != null)
-                {
-                    Snackbar.Make(CurrentFocus, "Refreshing...", Snackbar.LengthLong).SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
-                    web.RefreshDrawableState();
-                }
-                return true;
-            }
+
             return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View)sender;
+            Snackbar.Make(view, "Opening Browser", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+
+            LaunchBrowser();
+
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
